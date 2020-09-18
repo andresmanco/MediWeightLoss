c761ef4c830e38d3e790df8173365f42284eb470
Andres Manco <andresmanco@gmail.com>
2020-09-18 16:38:32 -0500

    delete

--- python_api/python_api/products/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import Product
-from rest_framework import serializers
-
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'code', 'product_type', 'cost', 'description', 'pushed_product', 'callback', 'category')
--- python_api/python_api/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from products.views import *
-
-urlpatterns = [
-url(r'^products/$', ProductList.as_view(), name = 'products')
-]
-
-app_name = 'product'
